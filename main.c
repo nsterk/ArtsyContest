da29a7f9686e97dd42f0b8d5fb5dfa0cf8f33dcb
Naomi <nsterk@student.codam.nl>
2020-12-22 11:34:18 +0100

    more colours, high speed bc of checking

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: nsterk <nsterk@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/12/21 17:36:00 by nsterk        #+#    #+#                 */
-/*   Updated: 2020/12/22 10:33:13 by nsterk        ########   odam.nl         */
+/*   Updated: 2020/12/22 11:33:40 by nsterk        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,30 +54,36 @@ void setColour(colour)
 	if (colour == 0)
 		printf("\e[0;38;5;88m");
 	else if (colour == 1)
-		printf("\e[0;38;5;125m");
-	else if (colour == 2)
 		printf("\e[0;38;5;162m");
+	else if (colour == 2)
+		printf("\e[0;38;5;201m");
 	else if (colour == 3)
-		printf("\e[0;38;5;199m");
-	else if (colour == 4)
-		printf("\e[0;38;5;128m");
-	else if (colour == 5)
 		printf("\e[0;38;5;93m");
+	else if (colour == 4)
+		printf("\e[0;38;5;57m");
+	else if (colour == 5)
+		printf("\e[0;38;5;21m");
 	else if (colour == 6)
-		printf("\e[0;38;5;62m");
-	else if (colour == 7)
 		printf("\e[0;38;5;27m");
+	else if (colour == 7)
+		printf("\e[0;38;5;43m");
 	else if (colour == 8)
-		printf("\e[0;38;5;25m");
+		printf("\e[0;38;5;41m");
 	else if (colour == 9)
-		printf("\e[0;38;5;23m");
+		printf("\e[0;38;5;83m");
 	else if (colour == 10)
-		printf("\e[0;38;5;22m");
+		printf("\e[0;38;5;227m");
+	else if (colour == 11)
+		printf("\e[0;38;5;214m");
+	else if (colour == 12)
+		printf("\e[0;38;5;202m");
+	else if (colour == 13)
+		printf("\e[0;38;5;196m");
 }
 
 int		changeColour(int colour)
 {
-	if (colour < 10)
+	if (colour < 13)
 		colour++;
 	else
 		colour = 0;
@@ -88,7 +94,7 @@ int	main(void)
 {
 	char	**pic;
 	int		x = 2;
-	int		y = 3;
+	int		y = 1;
 	int		timer = 25;
 	int		colour = 0;
 	                               
@@ -108,7 +114,7 @@ int	main(void)
 			printf("\e[1;1H\e[2J");
 			setColour(colour);
 			putPic(pic, x, y);
-			usleep(67000);
+			usleep(17000);
 			if (y > 40)
 			{
 				y = -y;
@@ -125,7 +131,7 @@ int	main(void)
 			printf("\e[1;1H\e[2J");
 			setColour(colour);
 			putPic(pic, x, y);
-			usleep(67000);
+			usleep(17000);
 			if (y > 40)
 			{
 				y = -y;
